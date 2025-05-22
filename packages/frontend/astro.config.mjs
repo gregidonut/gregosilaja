d3902b6bb2ee4500c09a4f7b6916eeb84308a197
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-05-22 22:42:43 +0800

    feat: add site name
    i don't know how astro will use this without a sitemap but it's probably
    better to add it now

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -8,6 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://gregosilaja.cc",
     integrations: [compressor(), react()],
     adapter: aws({
         responseMode: "stream",
