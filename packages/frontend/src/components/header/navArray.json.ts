6c950b5071e59b38bc4f25c8805e4e96c8944560
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-05-22 23:34:25 +0800

    feat: add some pages to the nav section
    all are blank right now except for the cv page that doesn't yet have
    permissions from it's iframe to my cv site

--- packages/frontend/src/components/header/navArray.json.ts
+++ packages/frontend/src/components/header/navArray.json.ts
@@ -2,5 +2,11 @@ type navItem = {
     name: string;
     url: string;
 };
-const navArray: readonly navItem[] = [{ name: "Home", url: "/" }];
+const navArray: readonly navItem[] = [
+    { name: "Home", url: "/" },
+    { name: "Portfolio", url: "/portfolio" },
+    { name: "Blog", url: "/blog" },
+    { name: "About Me", url: "/about" },
+    { name: "CV", url: "/cv" },
+];
 export default navArray;
